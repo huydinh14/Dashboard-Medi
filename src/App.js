d64a06a7cfefc3598a639920c2e8add25dd507e4
Zira <zira.dinh@emagegroup.com>
2023-02-13 21:30:21 +0700

    update structure

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>Hellooooooooooo Dashboard</div>
+  );
+}
+
+export default App;
---
+++ src/component/siderbar/Siderbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Siderbar = () => {
+  return (
+    <div>Siderbar neee</div>
+  )
+}
+
+export default Siderbar
