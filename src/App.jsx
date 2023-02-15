d6045954ed965cb21e150e59ea116cdd648c110c
huydinh14 <huydinhse@gmail.com>
2023-02-15 17:08:56 +0700

    update chart and table

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Blank from './pages/Blank'
 import Dashboard from './pages/Dashboard'
 import MainLayout from './layout/MainLayout'
 import './assets/css/index.css';
+import './assets/css/grid.css'
+import './assets/css/theme.css'
 
 function App() {
     return (
@@ -10,11 +12,14 @@ function App() {
             <Routes>
                 <Route path="/" element={<MainLayout />}>
                     <Route index element={<Dashboard />} />
-                    <Route path="orders" element={<Blank />} />
-                    <Route path="products" element={<Blank />} />
-                    <Route path="customers" element={<Blank />} />
-                    <Route path="settings" element={<Blank />} />
-                    <Route path="stats" element={<Blank />} />
+                    <Route path="hearth_beat_iot" element={<Blank />} />
+                    <Route path="patients" element={<Blank />} />
+                    <Route path="doctors" element={<Blank />} />
+                    <Route path="condition_rule" element={<Blank />} />
+                    <Route path="pathogens" element={<Blank />} />
+                    <Route path="hospitals" element={<Blank />} />
+                    <Route path="media_records" element={<Blank />} />
+                    <Route path="analytics" element={<Blank />} />
                 </Route>
             </Routes>
         </BrowserRouter>
--- src/component/topbar/Topbar.jsx
+++ src/component/topbar/Topbar.jsx
@@ -2,15 +2,40 @@ import React from "react";
 import "./topbar.css";
 import Dropdown from "../dropdown/Dropdown";
 
-import notifications from "../../assets/JsonData/notification.json"
+import notifications from "../../assets/JsonData/notification.json";
 import { Link } from "react-router-dom";
+import user_image from "../../assets/images/noAvatar.png";
+import user_menu from "../../assets/JsonData/user_menus.json";
+
+const current_user = {
+  display_name: "Huy Dinh",
+  image: user_image,
+};
 
 const renderNotificationItem = (item, index) => (
   <div className="notification-item" key={index}>
     <i className={item.icon}></i>
     <span>{item.content}</span>
   </div>
-)
+);
+
+const renderUseToggle = (user) => (
+  <div className="topbar_right-user">
+    <div className="topbar_right-user-image">
+      <img src={user.image} alt="user image" />
+    </div>
+    <div className="topbar_right-user-name">{user.display_name}</div>
+  </div>
+);
+
+const renderUserMenu = (item, index) => (
+  <Link to="/" key={index}>
+    <div className="notification-item">
+      <i className={item.icon}></i>
+      <span>{item.content}</span>
+    </div>
+  </Link>
+);
 
 const Topbar = () => {
   return (
@@ -21,15 +46,19 @@ const Topbar = () => {
       </div>
       <div className="topbar-right">
         <div className="topbar-right-item">
-          <Dropdown  icon="bx bx-user"/>
+          <Dropdown
+            customToggle={() => renderUseToggle(current_user)}
+            contentData={user_menu}
+            renderItems={(item, index) => renderUserMenu(item, index)}
+          />
         </div>
         <div className="topbar-right-item">
-          <Dropdown 
-          icon="bx bx-bell"
-          badge='12'
-          contentData={notifications}
-          renderItems={(item, index) => renderNotificationItem(item, index)}
-          renderFooter= {() => <Link to='/'>View All</Link>}
+          <Dropdown
+            icon="bx bx-bell"
+            badge="12"
+            contentData={notifications}
+            renderItems={(item, index) => renderNotificationItem(item, index)}
+            renderFooter={() => <Link to="/">View All</Link>}
           />
         </div>
         <div className="topbar-right-item">
